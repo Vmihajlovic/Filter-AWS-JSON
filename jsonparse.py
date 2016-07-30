09b360f5c47ca23d031b7ee21f40757bbfe00947
NJWRX <markjamesmeier@gmail.com>
2016-07-30 15:50:35 -0400

    Add files via upload

--- jsonparse.py
+++ jsonparse.py
@@ -1,5 +1,6 @@
 import json
 import requests
+from tabulate import tabulate
 
 #Program to retrieve AWS JSON file and print appropriate IP ranges
 #this will only retrieve EC2 services
@@ -9,19 +10,16 @@ data = response.json() #saves json to python object
 
 masterList = data['prefixes']
 
+regions = [["Asburn", "us-east-1"],["Palo Alto", "us-west-1"],["Portland","us-west-2"],["Singapore","ap-southeast-1"],["Sydney","ap-southeast-2"],["Tokyo","ap-northeast-1"],["Dublin","eu-west-1"],["Sao Paulo","sa-east-1"]]
+print("Region List: \n",tabulate(regions))
+      
 regionInput = input("enter your region: ")
 #serviceInput = input("enter your service: ")
 
-#EC2 is hardcoded below
+#EC2 service is hardcoded below
 for i, entry in enumerate(masterList):
     if entry['service'] == 'EC2' and entry['region'] == regionInput:
-        print(entry['service'],entry['ip_prefix'],entry['region']);
+#        print(entry['service'],entry['ip_prefix'],entry['region']);
+        print(entry['ip_prefix']);
     
 
-
-
-
-#object = dict (the json file)
-#Array = list (Prefixes)
-#This list is made up of objects(dicts?) which each have three strings
-#keys for the dicts that are in the list are ip_prefix, region and service
