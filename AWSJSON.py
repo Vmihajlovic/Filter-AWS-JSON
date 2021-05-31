7c9ee45cea90468b83223024e0efa5b1c702a9e0
vmihajlovic <mihajlovicviktor@gmail.com>
2021-05-31 17:02:44 +0200

    Update AWSJSON.py

--- AWSJSON.py
+++ AWSJSON.py
@@ -8,7 +8,6 @@ from tabulate import tabulate
 url = 'https://ip-ranges.amazonaws.com/ip-ranges.json'
 response = requests.get(url)
 data = response.json()
-f = open("IPranges.txt","w+")
 # Retrieve the sync token from the json file, which is epoch time
 # convert it to a readable date time
 epochTime = int(data['syncToken'])
@@ -32,6 +31,7 @@ regions = [["Ashburn", "us-east-1"], ["Palo Alto", "us-west-1"],
 print("This list was last updated: ", convertedTime)
 print("Region List: \n", tabulate(regions))
 regionInput = input("enter your region code (i.e. us-east-1): ")
+f = open("IPranges.txt","w+")
 
 # EC2 service is hardcoded below
 for i, entry in enumerate(addressData):
